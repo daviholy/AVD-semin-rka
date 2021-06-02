314fc9521e66cf3b15d3ebb9f2fcbd831e866677
MGebic <84675558+MGebic@users.noreply.github.com>
2021-06-02 17:22:17 +0200

    Update main.m

--- main.m
+++ main.m
@@ -7,3 +7,10 @@ cd ..
 map = h_parseHeader("data/drive06.hea","EMG");
 data = h_read("data/drive06.dat",map);
 plot(data)
+
+iemg = iemg_f(data); % iemg value from data
+data_part = data(1:100); % data from 1 to 100
+iemg_part = iemg_f(data_part); %iemg value from data 1-100
+
+disp(iemg);
+disp(iemg_part); 
