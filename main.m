8ea4661d581964a2354bf61a8edeb780cecaa135
daviholy <david.holy225@gmail.com>
2021-06-06 14:33:06 +0200

    opravena chyba způsobena pojmenováním proměnné

--- main.m
+++ main.m
@@ -9,9 +9,9 @@ map = h_parseHeader("data/drive06.hea","EMG");
 data = h_read("data/drive06.dat",map);
 plot(data)
 
-iemg = iemg_f(data); % iemg value from data
+iemg_res = iemg_f(data); % iemg value from data
 data_part = data(1:100); % data from 1 to 100
 iemg_part = iemg_f(data_part); %iemg value from data 1-100
 
-disp(iemg);
+disp(iemg_res);
 disp(iemg_part); 
